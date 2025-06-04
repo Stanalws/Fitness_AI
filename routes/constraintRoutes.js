a5e0deeac4ad6a0bc7a4f53bcc0a0f3659907d9e
Stanalws <9sten9f@gmail.com>
2025-06-04 09:49:33 +0300

    Delete routes directory

--- routes/constraintRoutes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import { listConstraints } from '../controllers/constraintController.js';
-
-const router = Router();
-router.get('/', listConstraints);
-
-export default router;
--- routes/exerciseRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { fetchAllExercises } from '../controllers/exerciseController.js';
-
-const router = express.Router();
-
-router.get('/', fetchAllExercises);
-
-export default router;
